46c7220fe4ae0e65a292c052c8cbd36d4d1793e5
malikadan212 <124624936+malikadan212@users.noreply.github.com>
2025-04-19 02:00:14 +0500

    Delete petro-pulse directory

--- petro-pulse/app/customer/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-// app/customer/dashboard/page.js
-export default function CustomerDashboard() {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-        <h1 className="text-4xl font-bold">Customer Dashboard</h1>
-      </div>
-    );
-  }
